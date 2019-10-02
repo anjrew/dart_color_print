6eea6e611a759bc431ce9e0d1df11cd80a6d73c3
earyzhe <earyzhe@gmail.com>
2019-10-02 10:40:38 +0200

    Changed so works on everything other than iOS

--- lib/src/dart_color_print_base.dart
+++ lib/src/dart_color_print_base.dart
@@ -2,32 +2,32 @@ import 'dart:io';
 import 'package:ansicolor/ansicolor.dart';
 
 void printInfo(dynamic consoleLog) {
-  AnsiPen pen = new AnsiPen()..blue();
-  print(Platform.isAndroid ? pen(consoleLog.toString()): consoleLog);
+  AnsiPen pen = AnsiPen()..blue();
+  print(!Platform.isIOS? pen(consoleLog.toString()): consoleLog);
 }
 
 void printWarning(dynamic consoleLog) {
-  AnsiPen pen = new AnsiPen()..yellow();
-  print(Platform.isAndroid ? pen(consoleLog.toString()): consoleLog);
+  AnsiPen pen = AnsiPen()..yellow();
+  print(!Platform.isIOS? pen(consoleLog.toString()): consoleLog);
 }
 
 void printError(dynamic consoleLog) {
-  AnsiPen pen = new AnsiPen()..red(bg: true);
-  print(Platform.isAndroid ? pen(consoleLog.toString()): consoleLog);
+  AnsiPen pen = AnsiPen()..red(bg: true);
+  print(!Platform.isIOS? pen(consoleLog.toString()): consoleLog);
 }
 
 void printSuccess(dynamic consoleLog) {
-  AnsiPen pen = new AnsiPen()..green();
-  print(Platform.isAndroid ? pen(consoleLog.toString()): consoleLog);
+  AnsiPen pen = AnsiPen()..green();
+  print(!Platform.isIOS? pen(consoleLog.toString()): consoleLog);
 }
 
 void printPurple(dynamic consoleLog) {
-  AnsiPen pen = new AnsiPen()..xterm(091, bg: true);
-  print(Platform.isAndroid ? pen(consoleLog.toString()): consoleLog);
+  AnsiPen pen = AnsiPen()..xterm(091, bg: true);
+  print(!Platform.isIOS ? pen(consoleLog.toString()): consoleLog);
 }
 
 void printPink(dynamic consoleLog) {
-  AnsiPen pen = new AnsiPen()..xterm(013);
-  print(Platform.isAndroid ? pen(consoleLog.toString()): consoleLog);
+  AnsiPen pen = AnsiPen()..xterm(013);
+  print(!Platform.isIOS? pen(consoleLog.toString()): consoleLog);
 }
 
